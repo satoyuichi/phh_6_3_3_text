4008aa54fc1f06412f241f491f7c134053ee9ef8
Yuichi Sato <y_sato@nnn.ed.jp>
2017-06-26 16:52:59 +0900

    First commit

---
+++ text.js
@@ -0,0 +1,4 @@
+document.addEventListener ('DOMContentLoaded', function () {
+  document.getElementById ('result_text').textContent = '<a href="http://www.wings.msn.to/">WINGS プロジェクト</a>';
+  document.getElementById ('result_html').innerHTML = '<a href="http://www.wings.msn.to/">WINGS プロジェクト</a>';
+}, false);
