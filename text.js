ba487aaa810e535df3c6f5dc5c2045fe2ac8c350
Yuichi Sato <y_sato@nnn.ed.jp>
2017-07-03 15:48:41 +0900

    リスト6−17を反映

--- text.js
+++ text.js
@@ -1,4 +1,7 @@
 document.addEventListener ('DOMContentLoaded', function () {
-  document.getElementById ('result_text').textContent = '<a href="http://www.wings.msn.to/">WINGS プロジェクト</a>';
-  document.getElementById ('result_html').innerHTML = '<a href="http://www.wings.msn.to/">WINGS プロジェクト</a>';
+  document.getElementById ('btn').addEventListener ('click', function () {
+    let name = document.getElementById ('name');
+    let result = document.getElementById ('result');
+    result.innerHTML = 'こんにちは、' + name.value + 'さん！';
+  }, false);
 }, false);
