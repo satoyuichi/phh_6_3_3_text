a1fc5aa929a9e3d9815c21a20ac9bb4029303cfd
Yuichi Sato <y_sato@nnn.ed.jp>
2017-07-03 15:01:04 +0900

    リスト6−16

--- text.js
+++ text.js
@@ -1,4 +1,5 @@
 document.addEventListener ('DOMContentLoaded', function () {
-  document.getElementById ('result_text').textContent = '<a href="http://www.wings.msn.to/">WINGS プロジェクト</a>';
-  document.getElementById ('result_html').innerHTML = '<a href="http://www.wings.msn.to/">WINGS プロジェクト</a>';
+  let list = document.getElementById ('list');
+  console.log (list.innerHTML);
+  console.log (list.textContent);
 }, false);
